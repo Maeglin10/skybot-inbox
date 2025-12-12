b7d7ab558bb4f9bdf89ef80010b5ea690fd0b059
Maeglin10 <v.milliand@gmail.com>
2025-12-12 18:07:59 +0100

    chore(core): clean NestJS bootstrap + Prisma runtime stable

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super();
+    const url = process.env.DATABASE_URL;
+    if (!url) throw new Error('DATABASE_URL is missing');
+
+    const adapter = new PrismaPg({ connectionString: url });
+    super({ adapter });
   }
 
   async onModuleInit() {
