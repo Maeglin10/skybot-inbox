2aa31a0345dd38b63243b52466db8ac87f1e440c
Maeglin10 <v.milliand@gmail.com>
2025-12-13 14:38:02 +0100

    fix(core): handle bootstrap promise

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import 'dotenv/config';
-
-import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
-bootstrap().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+void bootstrap();
